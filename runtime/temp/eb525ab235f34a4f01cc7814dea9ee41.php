844a74fc095485d8b345dfa836ef04f7cb17b2ea
fengshaw1 <xiaofrank77@gmail.com>
2022-08-20 18:34:38 +0800

    initial commit

---
+++ runtime/temp/eb525ab235f34a4f01cc7814dea9ee41.php
@@ -0,0 +1,14 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:103:"C:\Users\frank\Desktop\estate-init\estate-init\public/../application/admin\view\dashboard\activity.html";i:1587707196;}*/ ?>
+
+<ul class="nav nav-stacked">
+    <?php if(is_array($activity) || $activity instanceof \think\Collection || $activity instanceof \think\Paginator): if( count($activity)==0 ) : echo "" ;else: foreach($activity as $key=>$vo): ?>
+    <li>
+        <a href="javascript:void(0);" src="service/activity/detail/ids/<?php echo $vo['id']; ?>">
+            <span class="text"><?php echo $vo['title']; ?></span>
+            <span class="text" style="float:right;"><?php echo date("Y-m-d H:i:s",$vo['create_time']); ?></span>
+        </a>
+    </li>
+    <?php endforeach; endif; else: echo "" ;endif; if(empty($activity) || (($activity instanceof \think\Collection || $activity instanceof \think\Paginator ) && $activity->isEmpty())): ?>
+    <li>暂无记录</li>
+    <?php endif; ?>
+</ul>
